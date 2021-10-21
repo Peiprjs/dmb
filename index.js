4bb0dadd23f4b3f02f51d475b50dab80586d7d2c
Peipr <proca001@jvvgirona.eu>
2021-10-21 09:31:13 +0200

    Start

---
+++ index.js
@@ -0,0 +1,15 @@
+/////////////////Required modules///////////////////
+const fs = require('fs');
+const Discord = require("discord.js");
+const config = require("./config.json");
+const random = require('random')
+const cliProgress = require(
+    'cli-progress');
+let Trello = require("trello");
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
