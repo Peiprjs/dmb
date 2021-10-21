ab1dcf6f20e03e5abc3709006a31eacd2b2f3342
Peipr <proca001@jvvgirona.eu>
2021-10-21 10:35:05 +0200

    Okay here it all begins

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-/////////////////Required modules///////////////////
+/////////////////Modules, logins///////////////////
 const fs = require('fs');
-const Discord = require("discord.js");
-const config = require("./config.json");
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const random = require('random')
-const cliProgress = require(
-    'cli-progress');
+const cliProgress = require('cli-progress');
 function sleep(milliseconds) {
     const date = Date.now();
     let currentDate = null;
     do {
         currentDate = Date.now();
     } while (currentDate - date < milliseconds);
-};
-console.log("Gay")
+}
+/////////////////Once ready///////////////////
+client.once('ready', () => {
+    console.log('Ready!');
+    client.login(token)});
