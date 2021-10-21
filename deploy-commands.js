85cd17ddeeca8951d41f4be1954a823958219acc
Peipr <proca001@jvvgirona.eu>
2021-10-21 11:45:38 +0200

    Staging slash commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,12 +1,12 @@
+/////////////////Modules///////////////////
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
-
+/////////////////Commands definition///////////////////
 const commands = [
-    new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-    new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-    new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+    new SlashCommandBuilder().setName('ping').setDescription('Replies with the ping that the bot has at that moment!'),
+
 ]
     .map(command => command.toJSON());
 
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /////////////////ND Modules///////////////////
+// noinspection SpellCheckingInspection
+
 const fs = require('fs');
 const random = require('random')
 function sleep(milliseconds) {
@@ -16,5 +18,19 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.once('ready', () => {
     console.log('Ready!');
 });
-
+/////////////////Once interaction///////////////////
+// noinspection SpellCheckingInspection
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+    const { commandName } = interaction;
+/////////////////Spagheetto Redireccto///////////////////
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'server') {
+        await interaction.reply('Server info.');
+    } else if (commandName === 'user') {
+        await interaction.reply('User info.');
+    }
+});
+/////////////////LogMeIn Discordi///////////////////
 client.login(token);
