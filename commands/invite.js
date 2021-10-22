1e8707674ad65eee574ed2a78c1f004212c21d50
Peipr <proca001@jvvgirona.eu>
2021-10-22 02:38:32 +0200

    .

--- commands/invite.js
+++ commands/invite.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('invites')
+        .setName('invite')
         .setDescription('Replies with the bot invitation!'),
     async execute(interaction) {
-        await interaction.reply({ content: `https://discord.com/oauth2/authorize?client_id=900664144418734080&permissions=3149064&scope=applications.commands%20bot`, ephemeral: true });
+        await interaction.reply({ content: `**Invite me at**: https://discord.com/oauth2/authorize?client_id=900664144418734080&permissions=3149064&scope=applications.commands%20bot`, ephemeral: true });
     },
 };
